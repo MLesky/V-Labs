9f0ef181b06cd7cc747a863f5d239916409391b3
unknown <pamelafugua@gmail.com>
2023-04-20 06:54:23 +0200

    added forms

--- src/pages/Experiment.js
+++ src/pages/Experiment.js
@@ -7,11 +7,20 @@ const Experiment = () => {
     return(
         <div>
             <h1>The Experiment</h1>
-            <iframe src={location.state.url}
-                    width="800"
-                    height="600"
-                    allowfullscreen>
-            </iframe>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row"
+                }}
+            >
+                <iframe src={location.state.url}
+                        width="800"
+                        height="600"
+                        allowfullscreen>
+                    Loading…
+                </iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdZZF_u1-LyokRBaPwryO5eKcW4OdT-BlfBTSKlA07Z6X-2JQ/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
     )
 }
