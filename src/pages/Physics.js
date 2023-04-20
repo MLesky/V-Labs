599eb699671e383f28a7ca8aea909c17793d1133
unknown <pamelafugua@gmail.com>
2023-04-20 07:15:44 +0200

    some more changes

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -6,7 +6,20 @@ import { IMAGES } from '../utils';
 export default function CheckboxListSecondary() {
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap"
+      }}
+    >
+      {/* https://phet.colorado.edu/sims/html/circuit-construction-kit-ac-virtual-lab/latest/circuit-construction-kit-ac-virtual-lab_en.html */}
+      <ExpCard 
+            image={IMAGES.physics}
+            url={"https://phet.colorado.edu/sims/html/circuit-construction-kit-ac-virtual-lab/latest/circuit-construction-kit-ac-virtual-lab_en.html"}
+            title = {"Circuit Construction Kit"}
+            description = {"Circuit Construction Kit: AC - Virtual Lab"}
+        />
         <ExpCard 
             image={IMAGES.physics}
             url={"https://phet.colorado.edu/sims/html/calculus-grapher/latest/calculus-grapher_en.html"}
