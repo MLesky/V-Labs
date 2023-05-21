482c5298ca0fcd7aeb7dfb327b2238b325da67f2
MbLespa <mbahlesky2@gmail.com>
2023-05-21 18:56:40 +0100

    logo

--- src/assets/logo/logo.js
+++ src/assets/logo/logo.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 
-const Logo = ({size = '80px'}) => {
+const Logo = ({size = '40px'}) => {
   return <img src={logo} 
-  alt="v-labs" width={size} />;
+  alt="v-labs" width={size} style={{margin: '0 10px'}}/>;
 };
 
 export default Logo;
